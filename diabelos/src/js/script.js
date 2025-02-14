81493a04ef31c03b217d26b241c2c2751899b627
Konstantin Alekseev <alexcseev1990@gmail.com>
2025-02-14 16:43:12 +0200

    fix

--- diabelos/src/js/script.js
+++ diabelos/src/js/script.js
@@ -21,6 +21,38 @@ var landingFunctions = {
 			e.preventDefault();
 		})
 
+		// $(window).on("scroll", function(e) {
+		// 	const height = $(document).outerHeight(true)
+		// 	if(height - 2000 < $(window).scrollTop()) {
+		// 		$(".fixed__btn").fadeOut(300)
+		// 	} else {
+		// 		$(".fixed__btn").fadeIn(300)
+		// 	}
+		// })
+
+		function showBtn() {
+			var $element = $('.card__section');
+
+			$(window).scroll(function() {
+				var scroll = $(window).scrollTop() + $(window).height();
+				var offset = $element.offset().top + $element.height();
+				var bodyOffset = $("body").offset().top + $("body").height();
+
+				if ((scroll > offset + 400 || scroll < offset - $element.height() - 200)) {
+					if(scroll + 800 > bodyOffset) {
+						$(".fixed__btn").hide();
+					} else {
+						$(".fixed__btn").show()
+					}
+				} else {
+					$(".fixed__btn").hide()
+				}
+
+			});
+		}
+
+		showBtn()
+
 		// $('[data-fancybox]').fancybox({
 		// 	loop: true,
 		// 	infobar: false,
@@ -133,8 +165,6 @@ var landingFunctions = {
 
 		// $(".date__1").text(getDate(-5));
     	// $(".date").text(getDate(2));
-
-		$(".header__date").text(getDate(0));
 
 		$(".date-1").text(getDate(0));
 		$(".date-2").text(getDate(-1));
