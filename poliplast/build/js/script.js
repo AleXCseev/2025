25ac876eb2620a8f77de0314324fcf63e8bd6d6e
Konstantin Alekseev <alexcseev1990@gmail.com>
2025-07-19 18:28:11 +0300

    fix

--- poliplast/build/js/script.js
+++ poliplast/build/js/script.js
@@ -1,55 +1,11 @@
 var landingFunctions = {
 	init: function() {
 		this.initLibraris()
+		this.navigation()
 	}, 
 
 	initLibraris: function() {
 		
-		$('[href*="#"]').on('click', function (e) {
-			var fixedOffset = 20;
-			var cardHeight = $(".card").outerHeight(false)
-			var windowHeight = $(window).height()
-
-			$('html, body')
-			.stop()
-			.animate({ scrollTop: $(this.hash).offset().top + fixedOffset + (cardHeight - windowHeight)}, 1000);
-			// .animate({ scrollTop: $(this.hash).offset().top + fixedOffset}, 1000);
-			e.preventDefault();
-		})
-
-		$(".header__item").hover(
-			function() {
-				const text = $(this).find(".header__item-text").text()
-				setTimeout(() => {
-					$(".header__info").text(text).addClass("active")
-				}, 100)
-			},
-			function() {
-				$(".header__info").removeClass("active")
-			}
-		)
-
-		$(".nav__brend-item").click(function() {
-			$(".nav__brend-item").removeClass("active")
-			$(this).addClass("active")
-			const curentCategory = $(this).data("category");
-
-			$(".nav__category").removeClass("active")
-			$(`.nav__category[data-category=${curentCategory}]`).addClass("active")
-		})
-
-		const initialItem = $(".nav__category-item.active").data("item")
-
-		$(".nav__category-item").hover(
-			function() {
-				$(".nav__category-item").removeClass("active")
-				$(this).addClass("active")
-			},
-			function() {
-
-			}
-		)
-
 		// $('.review__slider').owlCarousel({
 		// 	items: 3,
 		// 	margin: 30,
@@ -97,6 +53,92 @@ var landingFunctions = {
 			backFocus: false,
 			hash: false,
 		});
+	},
+
+	navigation: function() {
+		$(".header__item").hover(
+			function() {
+				const text = $(this).find(".header__item-text").text()
+				setTimeout(() => {
+					$(".header__info").text(text).addClass("active")
+				}, 100)
+			},
+			function() {
+				$(".header__info").removeClass("active")
+			}
+		)
+
+		$(".catalog__modal").click(function() {
+			$(".nav__catalog").toggleClass("active");
+		})
+
+		$("body").on("keydown", function(e) {
+			const key = e.code
+			if(key === "Escape") {
+				$(".nav__catalog").removeClass("active");
+			}
+		})
+
+		$(".nav__brend-item").click(function() {
+			$(".nav__brend-item").removeClass("active")
+			$(this).addClass("active")
+			const curentCategory = $(this).data("category");
+
+			$(".nav__category").removeClass("active")
+			$(`.nav__category[data-category=${curentCategory}]`).addClass("active")
+
+			
+		})
+
+
+		$(".nav__category-item").hover(
+			function() {
+				$(this).closest(".nav__category").find(".nav__category-item").removeClass("active")
+				$(this).addClass("active")
+				const item = $(this).data("item")
+				$(this).closest(".nav__category").find(".nav__item").removeClass("active");
+				$(this).closest(".nav__category").find(`.nav__item[data-item=${item}]`).addClass("active");
+			},
+			function() {
+
+			}
+		)
+
+		$(".mobile__menu-btn").click(function() {
+			$("body").css("overflow", "hidden")
+			$(".mobile__menu").addClass("active")
+		})
+
+		$(".close__modal").click(function() {
+			$("body").css("overflow", "auto")
+			$(".mobile__menu").removeClass("active")
+		})
+
+		$(".mobile__catalog-btn").click(function() {
+			if(!$(this).hasClass("active")) {
+				$(this).addClass("active");
+				$(".mobile__catalog-list").removeClass("active").slideUp()
+				$(this).closest(".mobile__catalog-item").find(".mobile__catalog-list").slideDown().addClass("active")
+			} else {
+				$(".mobile__catalog-btn").removeClass("active")
+				$(".mobile__catalog-list").removeClass("active").slideUp()
+			}
+		})
+
+		$(".mobile__menu-btn").click(function() {
+			$("body").css("overflow", "hidden")
+			$(".mobile__menu").addClass("active")
+		})
+
+		$(".mobile__catalog-open").click(function() {
+			$("body").css("overflow", "hidden")
+			$(".mobile__catalog").addClass("active")
+		})
+
+		$(".mobile__catalog-close").click(function() {
+			$("body").css("overflow", "auto")
+			$(".mobile__catalog").removeClass("active")
+		})
 	},
 }
 
