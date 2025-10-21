584bc2120b4ccc874ebefdc05880f85ce1b5293f
Konstantin Alekseev <alexcseev1990@gmail.com>
2025-10-21 17:44:05 +0300

    add quest project

---
+++ quest/build/js/script.js
@@ -0,0 +1,44 @@
+var landingFunctions = {
+  init: function () {
+    this.initLibraries();
+  },
+
+  initLibraries: function () {
+    $('[href*="#"]').on("click", function (e) {
+      var fixedOffset = 0;
+
+      $("html, body")
+        .stop()
+        .animate({ scrollTop: $(this.hash).offset().top + fixedOffset }, 1000);
+      e.preventDefault();
+    });
+
+    AOS.init({
+      disable: function () {
+        if ($(window).width() <= 1080) {
+          return true;
+        }
+        return false;
+      },
+      once: true,
+      duration: 1000,
+      offset: 0,
+    });
+
+    $(window).resize(function () {
+      AOS.refresh();
+    });
+
+    $("[data-fancybox]").fancybox({
+      loop: true,
+      infobar: false,
+      animationEffect: false,
+      backFocus: false,
+      hash: false,
+    });
+  },
+};
+
+$(document).ready(function () {
+  landingFunctions.init();
+});
